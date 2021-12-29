9f96f22cd76ad6f3ee7d293dc17631d2a65916c9
Jacqueline <deanstro08@gmail.com>
2021-12-28 19:45:12 -0600

    Add favicon and edit index.html, style.css

--- script.js
+++ script.js
@@ -3,45 +3,45 @@ let randomNum = Math.floor(Math.random()*3 + 1);
 
 function computerPlay() {
   if (randomNum === 1) {
-    return "rock";
+    return "sun";
   } else if (randomNum === 2) {
-    return "paper";
+    return "cloud";
   } else if (randomNum === 3) {
-    return "scissors";
+    return "wind";
   }
 };
-
+ 
 let playerScore = 0;
 let computerScore = 0;
 
 //Function that plays one round using the player and computer's input and returns the winner of the round
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "It's a tie!";
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "It's a tie! There is only harmony!";
+  } else if (playerSelection === "sun" && computerSelection === "cloud") {
     computerScore += 1;  
-    return "Computer wins, paper beats rock! :(";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "Your sunshine has been blocked by clouds! :(";
+  } else if (playerSelection === "sun" && computerSelection === "wind") {
     playerScore += 1;
-    return "You win, rock beats scissors! :)";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "The sun has scared the wind into hiding, sunshine has prevailed! :)";
+  } else if (playerSelection === "cloud" && computerSelection === "sun") {
     playerScore += 1;
-    return "You win, paper beats rock! :)";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "Clouds have covered the warm sun, there is only nice shade! :)";
+  } else if (playerSelection === "cloud" && computerSelection === "wind") {
     computerScore += 1;
-    return "Computer wins, scissors beats paper! :(";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "Oh no! Your clouds have been puffed away! :(";
+  } else if (playerSelection === "wind" && computerSelection === "cloud") {
     playerScore += 1;
-    return "You win, scissors beats paper! :)";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "The wind has blown away the clouds, a win! :)";
+  } else if (playerSelection === "wind" && computerSelection === "sun") {
     computerScore += 1;
-    return "Computer wins, rock beats scissors! :(";
+    return "The wind doesn't like to be warm, defeat! :(";
   }; 
 
 }
 //Function that takes the player's input and makes it case-insensitive
 function playerInput() {
-  let playerChoose = "rock"
+  let playerChoose = "sun"
   let playerResponse = playerChoose.toLowerCase();
   return playerResponse;
 };
@@ -51,3 +51,10 @@ let playerSelection = playerInput();
 console.log(playRound(playerSelection, computerSelection));
 
 //Function that plays a five round game that keeps score and reports a winner/loser at the end
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection, computerSelection)
+        if (playerScore)
+    }
+}
